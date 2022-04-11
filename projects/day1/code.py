f8e51caaaed791017000c910b66a2ef8a43e6c18
Viktor <viktorjager1994@gmail.com>
2022-04-11 21:08:17 +0200

    files

---
+++ projects/day1/code.py
@@ -0,0 +1,13 @@
+travel_log = {
+    "France": {
+        "Paris": "Good",
+        "Lille": "Bad",
+        "Dijon": "Great",
+    },
+    "Germany": {
+        "Stratsburg": "Meh",
+        "Hamburg": "Wooopa",
+    },
+}
+
+print(travel_log)
