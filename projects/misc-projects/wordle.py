a343318f405fe06fa2e901441cc2409929518a21
Viktor <viktorjager1994@gmail.com>
2022-04-20 20:53:33 +0200

    wordle update

--- projects/misc-projects/wordle.py
+++ projects/misc-projects/wordle.py
@@ -2,18 +2,21 @@ from prettytable import PrettyTable, DOUBLE_BORDER
 from util import clear
 
 from data import words
+from art import logo
 import random
 
 
-
-searched_word = random.choice(words).upper()
-print(searched_word)
+CORRECT_WORD = random.choice(words).upper()
+print("Clue: " + CORRECT_WORD)
 
 table = PrettyTable()
 table.set_style(DOUBLE_BORDER)
-TABLE_HEADER = ["⚔", "W", "O", "R", "D", "L", "E"]
+TABLE_HEADER = ["⚔", "W", "Ö", "R", "D", "L", "E"]
 
 table.field_names = TABLE_HEADER
+clear()
+print(logo)
+print("Clue: " + CORRECT_WORD)
 print(table)
 
 
@@ -22,9 +25,9 @@ WORD_LENGTH = 6
 attempt = 1
 
 
-
 def is_valid_word(word, length):
     return len(word) == length and word.isalpha()
+
 
 def user_guess():
     while True:
@@ -32,81 +35,62 @@ def user_guess():
         if is_valid_word(guess, WORD_LENGTH):
             return guess
         else:
-            print ("That was not an acceptable word.")
-    
-def format(word, attempt):
-    formatted_answer = []
+            print(f"Invalid word! Make sure the word is {WORD_LENGTH} letters long")
 
+
+def format(word, CORRECT_WORD, attempt):
+    formatted_answer = []
     attempt_number = [f"{attempt}."]
+    formatted_answer.extend(attempt_number)
+
     word_list = [char for char in word]
 
-    formatted_answer.extend(attempt_number)
+    for char in range(len(word_list)):
+        if word_list[char] == CORRECT_WORD[char]:
+            word_list[char] = f"[{word_list[char]}]"
+        elif word_list[char] in CORRECT_WORD:
+            word_list[char] = f"({word_list[char]})"
+
     formatted_answer.extend(word_list)
 
     return formatted_answer
 
-def update_table(answer):
+
+def update_table(formatted_answer):
     table.add_row(formatted_answer)
+
+
+def update_board():
+    clear()
+    print(logo)
     print(table)
 
 
 while attempt <= MAX_ATTEMPTS:
-
     answer = user_guess()
-    formatted_answer = format(answer, attempt)
+    formatted_answer = format(answer, CORRECT_WORD, attempt)
     update_table(formatted_answer)
+    update_board()
 
-    if answer != searched_word and attempt == MAX_ATTEMPTS:
-        print("You lost!")
+    if answer != CORRECT_WORD and attempt == MAX_ATTEMPTS:
+        print(f"You lost! The correct word was: {CORRECT_WORD}")
         break
-    elif answer == searched_word:
+    elif answer == CORRECT_WORD:
         print("You won")
         break
     else:
         print("Wrong. Try again.")
 
     attempt += 1
-    
-
-
-
-
-''' rows = [ [ " " ] * 7 ] * 5
-answer_as_list = list(answer)
-rows[0] = answer_as_list '''
-
-
-
-""" table.field_names = ["⚔", "W", "O", "R", "D", "L", "E"]
-
-for row in rows: 
-    table.add_rows(
-        [
-            row
-        ]
-    ) 
-
-print(table)  """
-
-
-
-def wrap_char(type, char):
-    match type:
-        case "parenthesis":
-            return f"({char})"
-        case "bracket":
-            return f"[{char}]"
-        case "space" | _:
-            return f" {char} "
 
 
 # x Randomly select a 6 letter word, this is the correct word
-# Attempts = 6
+# x Attempts = 6
+#   (Nice to have: Choose attempts)
 
 
-# Ask for word from user
-# Word should only contains letters, exactly 6 letters in length
-# if word break rules, ask for new word
+# x Ask for word from user
+# x Word should only contains letters, exactly 'len' letters in length
+# x if word break rules, ask for new word
 
 # Print playboard
-
