02247bf9d11b9366dcd77535161e2df7e7e0af8c
Viktor <viktorjager1994@gmail.com>
2022-04-22 23:38:47 +0200

    snake game

---
+++ projects/day19/main.py
@@ -0,0 +1,45 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput(
+    title="Make your bet", prompt="Which turtle will win the race?"
+)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+
+is_race_on = False
+all_turtles = []
+
+
+y_pos = -70
+for color in range(len(colors)):
+
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[color])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_pos)
+    y_pos = y_pos + 30
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You won! The winning turtle was the {winning_color} one!")
+            else:
+                print(f"You lost! The winning turtle was the {winning_color} one!")
+
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+
+screen.exitonclick()
