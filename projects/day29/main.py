d4cb9fecd243962379365357c61c338c8ee7b3f1
Viktor <viktorjager1994@gmail.com>
2022-05-03 20:03:20 +0200

    pw-manager project, day 29

---
+++ projects/day29/main.py
@@ -0,0 +1,93 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    pw_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+    [password_list.append(choice(letters)) for _ in range(randint(8, 10))]
+    [password_list.append(choice(symbols)) for _ in range(randint(2, 4))]
+    [password_list.append(choice(numbers)) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+    password = "".join(password_list)
+    pw_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    pw = pw_entry.get()
+    data = f"{website} | {email} | {pw}\n"
+
+    if len(website) < 1 or len(pw) < 1:
+        messagebox.showerror(message="A field is empty!")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"Do you want to save the information?\n"
+            f"Website: {website}\n"
+            f"Email: {email}\n"
+            f"Password: {pw}",
+        )
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(data)
+            website_entry.delete(0, END)
+            pw_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Window
+window = Tk()
+window.title("PWManager")
+window.config(padx=20, pady=20)
+lock_img = PhotoImage(file="logo.png")
+window.iconphoto(False, lock_img)
+
+# Canvas
+canvas = Canvas(width=200, height=200, highlightthickness=0)  # width=200, height=200,
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = Label(text="Website:")
+email_label = Label(text="Email/Username:")
+pw_label = Label(text="Password:")
+
+website_label.grid(column=0, row=1)
+email_label.grid(column=0, row=2)
+pw_label.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry()
+email_entry = Entry()
+pw_entry = Entry()
+
+website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+pw_entry.grid(column=1, row=3, sticky="EW")
+
+website_entry.focus()
+email_entry.insert(0, "viktor@mail.com")
+
+# Buttons
+generate_pw_button = Button(text="Generate Password", command=generate_password)
+add_button = Button(text="Add", command=save)
+
+generate_pw_button.grid(column=2, row=3)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+window.mainloop()
